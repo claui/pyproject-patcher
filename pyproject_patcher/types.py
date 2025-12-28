9405071ffa2d68db18d2e380a48047393fb13f0c
Claudia Pellegrino <claui@users.noreply.github.com>
2025-12-28 16:47:32 +0100

    Add docstring to work around AutoAPI issue 489
    Without a docstring, AutoAPI would use the inherited docstring from
    `TypedDict`, which would violate indentation constraints:
    
    > ERROR: Unexpected indentation. [docutils]
    
    > WARNING: Block quote ends without a blank line; unexpected unindent. [docutils]
    
    For completeness, add a short docstring for `RequirementsContainer`, too.
    
    Link: https://github.com/readthedocs/sphinx-autoapi/issues/489

--- pyproject_patcher/types.py
+++ pyproject_patcher/types.py
@@ -4,12 +4,20 @@ from typing import NamedTuple, TypedDict
 
 
 class MarkerExpression(TypedDict):
+    """See also:
+    https://peps.python.org/pep-0508/#environment-markers
+    """
+
     op: str
     lhs: 'MarkerExpression | str'
     rhs: 'MarkerExpression | str'
 
 
 class RequirementsContainer(NamedTuple):
+    """See also:
+    https://packaging.python.org/en/latest/specifications/core-metadata/#core-metadata-provides-extra
+    """
+
     name: str
     extras: list[str] | None
     constraints: list[tuple[str, str]] | None
